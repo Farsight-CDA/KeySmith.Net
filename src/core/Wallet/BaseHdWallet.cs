32a0b88b9cd616f34f7e8c51c3d4d7c9e6be3a9a
Playwo <eliaswolf1609@gmail.com>
2024-11-18 22:07:51 +0100

    feat: Less opinionized BaseHdWallet

--- src/core/Wallet/BaseHdWallet.cs
+++ src/core/Wallet/BaseHdWallet.cs
@@ -8,28 +8,14 @@ public abstract class BaseHdWallet
 {
     private readonly Slip10Curve _curve;
 
-    private readonly byte[] _privateKey;
-    private readonly byte[] _publicKey;
-    private readonly byte[] _address;
-
     /// <summary>
     /// Private key of the wallet.
     /// </summary>
-    public ReadOnlySpan<byte> PrivateKey
-        => _privateKey;
-
+    protected readonly byte[] _privateKey;
     /// <summary>
     /// Public key of the wallet.
     /// </summary>
-    public ReadOnlySpan<byte> PublicKey
-        => _publicKey;
-
-    /// <summary>
-    /// The address of the wallet.
-    /// </summary>
-    public ReadOnlySpan<byte> Address
-        => _address;
-
+    protected readonly byte[] _publicKey;
     ///
     protected BaseHdWallet(Slip10Curve curve, ReadOnlySpan<byte> privateKey)
     {
@@ -45,19 +31,5 @@ public abstract class BaseHdWallet
         _privateKey = privateKey.ToArray();
         _publicKey = new byte[_curve.PublicKeyLength];
         curve.MakePublicKey(privateKey, _publicKey);
-
-        _address = new byte[AddressLength];
-        CalculateAddress(_address);
     }
-
-    /// <summary>
-    /// The byte length of an address.
-    /// </summary>
-    protected abstract int AddressLength { get; }
-
-    /// <summary>
-    /// Calculates the address of the wallet and writes it into the provided destination buffer.
-    /// </summary>
-    /// <param name="destination"></param>
-    protected abstract void CalculateAddress(Span<byte> destination);
 }
