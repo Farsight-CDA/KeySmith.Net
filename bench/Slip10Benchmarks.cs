6a5c694e6e2849c4621c1f8f4f0d4ec479d82ea5
Playwo <eliaswolf1609@gmail.com>
2024-11-18 18:12:12 +0100

    bench: Add ED25519 benchmarks

--- bench/Slip10Benchmarks.cs
+++ bench/Slip10Benchmarks.cs
@@ -1,6 +1,7 @@
 ﻿using BenchmarkDotNet.Attributes;
 using Keysmith.Net.BIP;
 using Keysmith.Net.EC;
+using Keysmith.Net.ED;
 using Keysmith.Net.SLIP;
 
 namespace Keysmith.Net.Bench;
@@ -12,7 +13,7 @@ public class Slip10Benchmarks
     private readonly byte[] _chainCodeBuffer = new byte[32];
 
     [Benchmark]
-    public (byte[], byte[]) Keysmith_Array_Slip10_Ethereum_512B()
+    public (byte[], byte[]) Keysmith_Array_Slip10_Secp256k1_Ethereum_512B()
         => Slip10.DerivePath(
             Secp256k1.Instance,
             TestData.Seed_512B,
@@ -20,12 +21,34 @@ public class Slip10Benchmarks
         );
 
     [Benchmark]
-    public bool Keysmith_Span_Slip10_Ethereum_512B()
+    public bool Keysmith_Span_Slip10_Secp256k1_Ethereum_512B()
     {
         Span<uint> path = stackalloc uint[5];
         BIP44.Ethereum(path);
         return Slip10.TryDerivePath(
             Secp256k1.Instance,
+            TestData.Seed_512B,
+            _keyBuffer,
+            _chainCodeBuffer,
+            path
+        );
+    }
+
+    [Benchmark]
+    public (byte[], byte[]) Keysmith_Array_Slip10_ED25519_Solana_512B()
+        => Slip10.DerivePath(
+            ED25519.Instance,
+            TestData.Seed_512B,
+            "m/44'/501'/0'/0'"
+        );
+
+    [Benchmark]
+    public bool Keysmith_Span_Slip10_ED25519_Solana_512B()
+    {
+        Span<uint> path = stackalloc uint[4];
+        BIP44.Solana(path);
+        return Slip10.TryDerivePath(
+            ED25519.Instance,
             TestData.Seed_512B,
             _keyBuffer,
             _chainCodeBuffer,
